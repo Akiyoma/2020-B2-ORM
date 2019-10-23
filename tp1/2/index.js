7be07c1f87746d8c2c98be9bd372d31675be6d95
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2019-10-23 11:15:14 +0200

    Update index.js

--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,7 +7,43 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+    let number = 1
+    let output = 0
+    let poids = 0
 
+    while (number <= input[0])
+    {
+      if (poids < 100)
+      {
+        poids += input[number]
+        number++
+        console.log('boucle1')
+      }
+      else if (poids > 100)
+      {
+        number--
+        poids = 0
+        output ++
+        console.log('boucle2')
+      }
+      else if (poids == 100)
+      {
+        output++
+        poids = 0
+        console.log('boucle3')
+        console.log(number)
+      }
+      if (number == input[0] + 1)
+      {
+        output++
+        console.log('boucle4')
+      }
+    }
+    
+
+    console.log(output)
+    console.log(number)
+    return output
     // AND HERE
   },
   verify: function (dataset, output) {
